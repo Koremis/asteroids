f91fe023ca4f3b3e007f5d51f83832c1460b2b74
koremis <koremis@proton.me>
2025-05-05 13:30:32 -0400

    Started adding player model

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from constants import *
+from player import *
 import pygame
 
 
-pygame.init()
-
-
 def main():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    pygame.init()
+    clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,8 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
