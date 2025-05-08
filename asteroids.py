b91418c8fab5c3b7c9d5ae302c881bcd99e7a120
koremis <koremis@proton.me>
2025-05-08 14:29:02 -0400

    Added template for asteroids

---
+++ asteroids.py
@@ -0,0 +1,9 @@
+import pygame
+
+from circleshape import CircleShape
+
+class Asteroids(CircleShape):
+    def __init(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(pygame.draw.circle):
